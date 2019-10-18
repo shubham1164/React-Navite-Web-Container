a5eca6900bf09796883a2007c9b1ac4bb5882c57
Shubham Singla <shubham.singla@feryservices.com>
2019-10-19 01:25:13 +0530

    Added Firebase for Android

--- webContainerApp/App.js
+++ webContainerApp/App.js
@@ -7,11 +7,37 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Alert } from 'react-native';
+import {Platform, StyleSheet, Text, View, Alert, Clipboard } from 'react-native';
 import { WebView } from 'react-native-webview';
+import type firebase from 'react-native-firebase';
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  componentDidMount(){
+    this.getFirebaseToken();
+  }
+
+  getFirebaseToken = () => {
+
+    try {
+      const enabled = await firebase.messaging().hasPermission();
+      if (enabled) {
+          // user has permissions
+          await firebase.messaging().requestPermission();
+          const fcmToken = await firebase.messaging().getToken();
+          // get the token from here and send to the Application server and store it permanently in the db 
+          console.log('Token: ', fcmToken)
+      } else {
+          // user doesn't have permission
+          alert('No permission')
+      }
+    } catch (e) {
+        // User has rejected permissions
+        console.log('err: ', e.message)
+    }
+  }
+
   onMessage = (event) => {
     const {title, message} = JSON.parse(event.nativeEvent.data)
     Alert.alert(
@@ -29,13 +55,13 @@ export default class App extends Component<Props> {
     console.log("requestHeader - "+requestHeader);
     return (
       <View style={styles.container}>
-        <WebView          
-          source={{ 
+        <WebView
+          source={{
             uri: sourceUri,
             headers: {requestHeader}
           }}
           useWebKit={true}
-          style={{ marginTop: 50 }} 
+          style={{ marginTop: 50 }}
           javaScriptEnabled={true}
           originWhitelist={['*']}
           onMessage={this.onMessage}
--- webContainerApp/android/app/src/main/java/com/webcontainerapp/MainApplication.java
+++ webContainerApp/android/app/src/main/java/com/webcontainerapp/MainApplication.java
@@ -7,10 +7,12 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage; // <-- Add this line
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // <-- Add this line
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseNotificationsPackage()); // <-- Add this line
+      packages.add(new RNFirebaseMessagingPackage()); // <-- Add this line
       return packages;
     }
 
